5a4c11906111f521455e57151704abf549f70030
Mirco Catalano <catalanomircosav@gmail.com>
2025-11-17 19:08:31 +0100

    Integrate SDL3 as vendored submodule and engine dependency. Closes #2

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "engine.h"
 
+#include <SDL3/SDL.h>
+
 void engine_print() {
+    if(!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "[Engine] Error occurred while initializing SDL3.\n";
+        return;
+    } else {
+        std::cout << "[Engine] SDL initialized correctly.\n";
+    }
     std::cout << "[Engine] ShadowFall engine initialized!\n";
 }
